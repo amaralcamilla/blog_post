d300049479bd496f2f6b8bf1b99140c6e55d882b
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-28 15:48:18 -0300

    feat: saves a new post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,7 +6,8 @@ const blogReducer = (state, action) => {
         case 'add_blogpost':
             return [...state, {
                 id: Math.floor(Math.random() * 99999),
-                title: `Blog Post #${state.length + 1}`
+                title: action.payload.title,
+                content: action.payload.content
             }];
         case 'delete_blogpost':
             return state.filter(blogPost => blogPost.id !== action.payload)
@@ -15,8 +16,8 @@ const blogReducer = (state, action) => {
     }
 }
 const addBlogPost = (dispatch) => {
-    return () => {
-        dispatch({type: 'add_blogpost'});
+    return (title, content) => {
+        dispatch({type: 'add_blogpost', payload: {title, content}});
     }
 }
 const deleteBlogPost = dispatch => {
