909ac6bdfe6c51d62c8b5463fe8f67679e1d3c04
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-28 12:17:27 -0300

    feat: adds deleteBlogPost function

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -8,6 +8,8 @@ const blogReducer = (state, action) => {
                 id: Math.floor(Math.random() * 99999),
                 title: `Blog Post #${state.length + 1}`
             }];
+        case 'delete_blogpost':
+            return state.filter(blogPost => blogPost.id !== action.payload)
         default:
             return state;
     }
@@ -17,5 +19,10 @@ const addBlogPost = (dispatch) => {
         dispatch({type: 'add_blogpost'});
     }
 }
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({type: 'delete_blogpost', payload: id})
+    };
+};
 
-export const {Context, Provider} = createDataContext(blogReducer, {addBlogPost}, []);
+export const {Context, Provider} = createDataContext(blogReducer, {addBlogPost, deleteBlogPost}, []);
