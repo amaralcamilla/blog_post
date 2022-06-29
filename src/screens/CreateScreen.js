9d020f51755f080ea0d6d3137298e3d87679a319
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-29 13:48:19 -0300

    refactor: extracts form logic

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,43 +1,14 @@
-import React, {useContext, useState} from "react";
-import {View, Text, TextInput, StyleSheet, Button} from "react-native";
+import React, {useContext} from "react";
+import {StyleSheet} from "react-native";
 import {Context} from '../context/BlogContext';
+import BlogPostForm from "../components/BlogPostForm";
 
 const CreateScreen = () => {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
     const {addBlogPost} = useContext(Context);
 
-    return (
-        <View>
-            <Text style={styles.label}>Enter Title:</Text>
-            <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)}/>
-            <Text style={styles.label}>Enter Content:</Text>
-            <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
-            <Button
-                title="Add Blog Post"
-                onPress={() => {
-                    addBlogPost(title, content, () => {
-                        //navigation.navigate('Index');
-                    });
-                }}
-            />
-        </View>
-    )
+    return <BlogPostForm/>
 };
 
-const styles = StyleSheet.create({
-    input: {
-        fontSize: 16,
-        borderWidth: 1,
-        borderColor: 'black',
-        margin: 5,
-        padding: 5
-    },
-    label: {
-        fontSize: 18,
-        marginBottom: 5,
-        marginLeft: 5,
-    }
-});
+const styles = StyleSheet.create({});
 
 export default CreateScreen;
